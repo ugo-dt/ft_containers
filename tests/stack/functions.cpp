d98f1d9c82e675096c8a86aad52701682b65bb33
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-05-23 22:06:34 +0200

    stack ok

--- tests/stack/functions.cpp
+++ tests/stack/functions.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 21:02:35 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/05/11 21:08:38 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/05/23 15:20:59 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	tests_stack_functions()
 		std::cout << "> size" << std::endl;
 		std::stack<int> s;
 		std::cout << "0. size: " << s.size() << std::endl;
-		for (int i=0; i<5; i++)
+		for (int i=0; i<50000000; i++)
 			s.push(i);
 		std::cout << "1. size: " << s.size() << std::endl;
 		s.pop();
